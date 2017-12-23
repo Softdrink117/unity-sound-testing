bb48e5a2f35fba4cd6d3139f14efd506535e4a22
Tobias Heinemann <tobyhein@gmail.com>
2017-12-23 13:13:19 -0800

    Folder cleanup, foundation of SFXSource
    - Cleaned up folder structure and file organization
    - Created foundation for SFXSource and SFX playback systems

---
+++ Assets/Scripts/Sound/SFX/SFXSource.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Softdrink{
+	// Class to contain Sound Effect data
+	[CreateAssetMenu(menuName = "Sound/SFX Source")]
+	public class SFXSource : ScriptableObject {
+
+		[TooltipAttribute("The name of this SFXSource. \nMostly used for human-readibility and tagging/meta purposes.")]
+		public string Name = "New SFX Source";
+
+		[TooltipAttribute("Audio clips to be used as sources. If more than one is included, one will be randomly selected each time the SFXSource is Played.")]
+		public AudioClip[] sources;
+
+		[TooltipAttribute("Playback settings for this SFXSource.")]
+		public SFXPlaybackSettings settings;
+
+	}
+}
---
+++ Assets/Scripts/Sound/SFX/SFXSourceIncludes.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Softdrink{
+	// Struct containing playback data for an SFXSource (or other randomizable audio)
+	[System.Serializable]
+	public struct SFXPlaybackSettings{
+
+		[Range(0f,1f)]
+		[TooltipAttribute("The playback volume for this sound effect.")]
+		public float volume;
+
+		[TooltipAttribute("How much volume randomization to apply when playing back.")]
+		public float volumeRandom;
+
+		[TooltipAttribute("How much pitch randomization to apply when playing back.")]
+		public float pitchRandomization;
+
+		public SFXPlaybackSettings(float volIn, float volRandIn, float pitchRandIn){
+			volume = volIn;
+			volumeRandom = volRandIn;
+			pitchRandomization = pitchRandIn;
+		}
+
+		public void Validate(){
+			if(volume < 0f) volume = 0f;
+		}
+	};
+}
